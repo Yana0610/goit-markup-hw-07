0701b84c494adc77c75660e790f754581e066059
Yana Marynchenko <canon357@gmail.com>
2023-02-09 19:55:03 +0100

    commit

--- js/menu.js
+++ js/menu.js
@@ -1,14 +1,26 @@
 (() => {
-    const refs = {
-      openMenuBtn: document.querySelector("[data-menu-open]"),
-      closeMenuBtn: document.querySelector("[data-menu-close]"),
-      menu: document.querySelector("[data-menu]"),
-    };
-  
-    refs.openMenuBtn.addEventListener("click", toggleMenu);
-    refs.closeMenuBtn.addEventListener("click", toggleMenu);
-  
-    function toggleModal() {
-      refs.menu.classList.toggle("is-hidden");
-    }
-  })();
+  const refs = {
+   openMenuBtn: document.querySelector(
+    "[data-menu-open]"
+   ),
+   closeMenuBtn: document.querySelector(
+    "[data-menu-close]"
+   ),
+  menu: document.querySelector("[data-menu]"),
+  body:document.querySelector("[body]"),
+};
+ 
+  refs.openMenuBtn.addEventListener(
+   "click",
+   toggleMenu
+  );
+  refs.closeMenuBtn.addEventListener(
+   "click",
+   toggleMenu
+  );
+ 
+  function toggleMenu() {
+   refs.menu.classList.toggle("is-hidden");
+   refs.body.classList.toggle("no-scroll");
+  }
+ })();
